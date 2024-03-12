1e9213a2528e44348b8aa8d242161eea43152b99
achilezweb@gmail.com <achilezweb@gmail.com>
2024-03-12 08:49:19 +0000

    Created posts index to list all blog posts and submit new blog post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post = Post::latest()->get();
+        return response()->json($post);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'body' => ['required', 'string'],
+            'user_id' => ['exists:users,id'],
+        ]);
+
+        $post = Post::create($validatedData);
+
+        return response()->json($post, 201);
+    }
+}
